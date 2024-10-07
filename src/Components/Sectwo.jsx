5c14b647efde2c2009d3f5631d215c1c2559bd3d
Patrick Ashi <patrickpeko1@gmail.com>
2024-10-07 09:31:12 +0100

    new

--- src/Components/Sectwo.jsx
+++ src/Components/Sectwo.jsx
@@ -13,7 +13,7 @@ const Sectwo = () => {
         <div className="group px-4 md:px-20 py-32 bg-[#3b82f6] hover:bg-emerald-100 text-gray-100">
             <div ref={ref} className={`w-full md:w-[700px]  transition-transform duration-100 ${isVisible ? 'animate-slideIn' : '-translate-x-1'}`}>
                 <button className="group-hover:bg-[#3b82f6] mb-6 bg-white text-xs font-oleo text-gray-800 group-hover:text-white py-1 px-2 rounded-tr-xl">WELCOME</button>
-                <h2 className="group-hover:text-[#3b82f6] text-3xl md:text-5xl mb-4 font-bold font-oleo group-hover:scale-105 duration-200">
+                <h2 className="group-hover:text-[#3b82f6] text-3xl md:text-5xl mb-4 font-bold font-yatra group-hover:scale-105 duration-200">
                     HOW ARE YOU DOING TODAY? ARE YOU OKAY?
                 </h2>
             </div>
